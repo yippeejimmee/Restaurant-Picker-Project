cea7b2decd4f513f7fd6f83f0a60458958b31f56
Joey B <101432570+joeytmab@users.noreply.github.com>
2022-04-30 11:11:45 -0700

    Joey (#11)
    * dog fact api
    
    * test

--- assets/script.js
+++ assets/script.js
@@ -33,11 +33,16 @@ var url = "http://dog-api.kinduff.com";
 
 
 
-fetch('https://cors-anywhere.herokuapp.com/http://dog-api.kinduff.com/api/facts').then(response => {
+fetch('http://dog-api.kinduff.com/api/facts?number=5', {
+    mode: 'no-cors'
+}).then(response => {
     console.log(response);
-    return response.json(); //nest response and allows us to format it
-})//then(e=> {
-    //e.data.forEach(item => {
+    //return response.json(); //nest response and allows us to format it
+}) .catch(error => console.log('error', error));
+
+//then(e=> {
+
+      //e.data.forEach(item => {
        // dogFactEl.innerHTML += item.
     //})
 
