362ca6b4ae53dbd58698d2d979ce11825935cc65
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-05-01 17:47:31 -0700

    HTML and CSS Updates (#17)
    * Pushing readme update
    
    * Testing branch updates and PR approval
    
    * Testing branch merging2
    
    * Pushing changes to HTML file to add form options for pricing and rating
    
    * adjusted price max to 13001300
    
    * Making modifications to index.html file addings styling and cleanign up layout
    
    * Pushing updates to development branch
    
    Co-authored-by: Joey Binas <joseph.t.binas@gmail.com>

--- assets/script.js
+++ assets/script.js
@@ -47,3 +47,18 @@ fetch('http://dog-api.kinduff.com/api/facts?number=5', {
     //})
 
 
+//DANIEL ADDING API SCRIPT TO FETCH Random Dog Picture
+//On page load trigger the API
+
+function fetchDogPicture() {
+    var fetchDogPictureEndpoint = "https://dog.ceo/api/breeds/image/random"; 
+    fetch(fetchDogPictureEndpoint, {
+    }).then(response => {
+        console.log(response);
+    }).catch(error => 
+        console.log("error", error));
+}
+
+$(document).ready(fetchDogPicture)
+
+//ENDING RANDOM DOG PICTURE SCRIPT
