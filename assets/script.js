e7841ce971616c29cfbdb38052d3eb9220ab9e8b
yippeejimmee <77139684+yippeejimmee@users.noreply.github.com>
2022-05-02 21:04:53 -0700

    Jim3 (#26)
    * change index and script
    
    * Added food search bar, modified CSS, updated Javascript local data

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 var searchForm = $('#search-form');
 var searchBtn = $('.searchBtn');
 // var searchQuery = ('#search-place');
-var searchQueryTwo = $('.form-input');
+var searchQueryPlace = $('.form-inputPlace');
+var searchQueryFood = $('.form-inputFood')
 var apiKey = "gJVmTi7vwWY--jKnwBsPJdLiPDsil3tcQzGmNEpsaoBkFKdkMwmTdiB_RCkLqnrExNMK-VW2twwvYqNssc1H8r25mJE0L-ZTnpq2xSa88h65tb8IzboCX_C1UHFrYnYx"
 var dogFactEl = document.getElementById("#dog-fact");
 
@@ -13,7 +14,7 @@ function addAddressInformationToPage () {
 function getLocationResults(e) {
     e.preventDefault();
 
-    var searchRequest = searchQueryTwo.val()
+    var searchRequest = searchQueryPlace.val()
     var myHeaders = new Headers();
     myHeaders.append("Authorization", "Bearer " + apiKey);
     console.log(searchRequest);
@@ -30,17 +31,49 @@ function getLocationResults(e) {
         })
         .then(function (data) {
             console.log(data);
-            $i = 0;
+            var i = 0;
+
+            // for (let index = 0; index < data.businesses.length; index++) {
+            //     const searchResult = {
+            //                     name: item.name,
+            //                     address: item.location.address1,
+            //                     picture: item.image_url,
+            //                 }
+
+            //     localStorage.setItem("Result" + [index], JSON.stringify(searchResult))
+
             data.businesses.forEach(function (item) {
                     const searchResult = {
                         name: item.name,
                         address: item.location.address1,
                         picture: item.image_url,
                     }
-                    localStorage.setItem(item.name, JSON.stringify(searchResult));
+                    localStorage.setItem("result" + [i], JSON.stringify(searchResult));
+                    i++;
                 })
                 .catch(error => console.log('error', error));
         })
+}
+
+function getRestaurant(selectionLocation) {
+    var foodRequest = searchQueryPlace.val()
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + apiKey);
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?=&${foodRequest}limit=10&location=${selectionLocation}`, requestOptions)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+        })
+
 }
 
 // function selectLocationFunction(e) {
